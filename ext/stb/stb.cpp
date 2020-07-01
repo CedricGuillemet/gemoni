7b81441b262a88a1cbb0757851cf2437f95190b2
Cedric Guillemet <cedric.guillemet@gmail.com>
2020-07-01 22:28:58 +0200

    nano

--- ext/stb/stb.cpp
+++ ext/stb/stb.cpp
@@ -2,3 +2,8 @@
 #include "stb_image_resize.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#define NANOSVG_ALL_COLOR_KEYWORDS // Include full list of color keywords.
+#define NANOSVG_IMPLEMENTATION     // Expands implementation
+#include "nanosvg.h"
+#define NANOSVGRAST_IMPLEMENTATION
+#include "nanosvgrast.h"
